e7ce6f97d46854233cb6c135934530cb2cd5b3e4
vikas-jakhar <vikashjakhar2004@gmail.com>
2024-03-13 15:40:27 +0530

    first commit

---
+++ assets/js/javascript.js
@@ -0,0 +1,35 @@
+const menuicon = document.querySelector(".menu-icon");
+const menulist = document.querySelector(".menu-list");
+const navbar = document.querySelector(".navbar-1");
+
+menuicon.addEventListener("click", function () {
+    menulist.classList.toggle("show-list");
+    navbar.classList.toggle("icon-menu");
+});
+
+
+$('.slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                arrows: false,
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,  
+                arrows: false,
+            }
+        }
+    ]
+});
